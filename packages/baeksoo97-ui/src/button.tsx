967983ea73edbd858e89a0c38895c2c059bf6efd
Lyla Baek <sooyoungb97@gmail.com>
2024-02-20 11:21:48 +0900

    button color change

--- packages/baeksoo97-ui/src/button.tsx
+++ packages/baeksoo97-ui/src/button.tsx
@@ -5,11 +5,8 @@ export interface ButtonProps
 
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
   return (
-    <button type="button" className="bg-gray-200 text-primary" {...other}>
+    <button type="button" className="bg-blue-600 text-primary" {...other}>
       {children}
-      <h1>header1</h1>
-      <h2>header2</h2>
-      <h3>header3</h3>
     </button>
   );
 }
