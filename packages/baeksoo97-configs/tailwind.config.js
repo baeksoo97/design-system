0c8cf3a633e4ccf2abaa0a4fa33812a5900e9fae
Lyla Baek <sooyoungb97@gmail.com>
2024-02-27 10:44:38 +0900

    configs

--- packages/baeksoo97-configs/tailwind.config.js
+++ packages/baeksoo97-configs/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     content: [
       "./stories/**/*.{js,ts,jsx,tsx}",
       "../../packages/baeksoo97-ui/src/**/*.{js,ts,jsx,tsx}", // Add the ui package
+      "../../packages/baeksoo97-ui-shadcn/src/**/*.{js,ts,jsx,tsx}", // Add the ui package
     ],
     theme: {
       extend: {
