a26ec1808f7fd21bc3b8a7e5818743d82ece2243
Lyla Baek <sooyoungb97@gmail.com>
2024-02-27 10:10:55 +0900

    add shadcn ui

---
+++ packages/baeksoo97-ui-shadcn/src/components/ui/index.tsx
@@ -0,0 +1,5 @@
+export {Accordion, AccordionItem, AccordionTrigger, AccordionContent} from './accordion';
+export {AlertDialog, AlertDialogPortal, AlertDialogOverlay,AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel} from './alert-dialog';
+export {Button, buttonVariants, type ButtonProps} from './button';
+export {Label} from './label';
+export {Input} from './input';
