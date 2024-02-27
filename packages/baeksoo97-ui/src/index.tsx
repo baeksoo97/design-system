091af92589017e453b7c1f073f65a98fab76f881
Lyla Baek <sooyoungb97@gmail.com>
2024-02-27 11:01:23 +0900

    ui

--- packages/baeksoo97-ui/src/index.tsx
+++ packages/baeksoo97-ui/src/index.tsx
@@ -1,1 +1,5 @@
-export { Button, type ButtonProps } from "./button";
+export {Accordion, AccordionItem, AccordionTrigger, AccordionContent} from './accordion';
+export {AlertDialog, AlertDialogPortal, AlertDialogOverlay,AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel} from './alert-dialog';
+export {Button, buttonVariants, type ButtonProps} from './button';
+export {Label} from './label';
+export {Input} from './input';
